ded60c6c1b7fd41e5ee49000bf2d642cee9d8b63
Александр Илюшин <the_ais@rambler.ru>
2020-04-21 19:40:55 +0300

    Отображение состояния ответа

--- quiz/src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ quiz/src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -2,9 +2,15 @@ import React from 'react';
 import classes from './AnswerItem.module.css';
 
 const AnswerItem = props => {
-    return (
+    const cls = [classes.AnswerItem]
+
+    if (props.state) {
+        cls.push(classes[props.state])
+    }
+
+    return  (
         <li 
-            className={classes.AnswerItem}
+            className={cls.join(' ')}
             onClick={() => props.onAnswerClick(props.answer.id)}
         >
             {props.answer.text}
