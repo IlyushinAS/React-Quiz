801bb5f52654b1bf8172be24c22d47b7fed95bbf
Александр Илюшин <the_ais@rambler.ru>
2020-04-23 14:15:06 +0300

    Подключение библиотеки axiox

--- quiz/src/containers/QuizList/QuizList.js
+++ quiz/src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './QuizList.module.css';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 export default class QuizList extends Component {
 
@@ -15,6 +16,13 @@ export default class QuizList extends Component {
                     </NavLink>
                 </li>
             )
+        })
+    }
+
+    componentDidMount() {
+        axios.get('https://react-quiz-78a5c.firebaseio.com/quiz.json').then(response => {
+            console.log(response);
+            
         })
     }
 
